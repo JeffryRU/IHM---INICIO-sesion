1e4453112bd5b9802d8e2ca77b3f3b68afda17ce
JeffryRU <165932162+JeffryRU@users.noreply.github.com>
2024-06-22 21:15:03 +0000

    a

---
+++ src/components/Register.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
+
+function Register() {
+    return (
+        <Container>
+            <h2>Register</h2>
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPasswordConfirm">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control type="password" placeholder="Confirm Password" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </Container>
+    );
+}
+
+export default Register;
